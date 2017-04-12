87e0a6564d8c4761d22c02ddcf349607f6118886
ShiningDan <zhuce.zyc@gmail.com>
2017-04-12 16:32:24 +0800

    finish drawline

--- src/js/drawtable.js
+++ src/js/drawtable.js
@@ -1,6 +1,5 @@
 window.onload = function() {
     let canvas = document.getElementById('chess-table');
-    let tablectx = canvas.getContext('2d');
     let {width, height} = document.getElementById('table').getBoundingClientRect();
     canvas.width = 2 * width;
     canvas.height = 2 * height;
@@ -11,39 +10,85 @@ window.onload = function() {
         left: '50%',
         transform: 'translate(-50%, -50%) scale(0.5)',
     });
-    drawtable(tablectx, width, height);
+    drawtable(canvas, width, height);
 }
 
-function drawtable(ctx, width, height) {
+function drawtable(canvas, width, height) {
     let xStep = width/20;
     console.log(xStep);
-    drawLine(ctx, '#000', 2*xStep, xStep, 18*xStep, xStep);
+    let leftTop = new Point2D(2*xStep, 2*xStep),
+    rightTop = new Point2D(18*xStep, 2*xStep),
+    leftBottom = new Point2D(2*xStep, 28*xStep),
+    rightBottom = new Point2D(18*xStep, 28*xStep);
+    let lineArray = []
+    for (let i = 1; i <= 6; i++) {
+        lineArray.push([2, 2*i, 18, 2*i]);
+    }
+    for (let i = 9; i <= 14; i++) {
+        lineArray.push([2, 2*i, 18, 2*i]);
+    }
+    for (let i = 0; i < 3; i++) {
+        lineArray.push([2+8*i, 2, 2+8*i, 28]);
+    }
+    for (let i = 0; i < 2; i++) {
+        lineArray.push([6+8*i, 2, 6+8*i, 12]);
+        lineArray.push([6+8*i, 18, 6+8*i, 28]);
+    }
+    lineArray.push([10, 4, 18, 8]);
+    lineArray.push([2, 4, 18, 12]);
+    lineArray.push([2, 8, 10, 12]);
+
+    lineArray.push([10, 4, 2, 8]);
+    lineArray.push([18, 4, 2, 12]);
+    lineArray.push([18, 8, 10, 12]);
+
+    lineArray.push([2, 22, 10, 26]);
+    lineArray.push([2, 18, 18, 26]);
+    lineArray.push([10, 18, 18, 22]);
+
+    lineArray.push([2, 22, 10, 18]);
+    lineArray.push([2, 26, 18, 18]);
+    lineArray.push([10, 26, 18, 22]);    
+    for (let i = 0; i < lineArray.length; i++) {
+        drawLine(canvas, '#000', lineArray[i][0]*xStep, lineArray[i][1]*xStep, lineArray[i][2]*xStep, lineArray[i][3]*xStep);
+    }
+    drawStation(canvas, '#424D37', '#538ED5', 2*xStep, 2*xStep, 2*xStep, xStep, 0);
 }
 
-function drawLine(ctx, color, x1, y1, x2, y2){
+function drawLine(canvas, color, x1, y1, x2, y2){
+    let ctx = canvas.getContext('2d');
     ctx.strokeStyle = color;
     ctx.beginPath();
+    [x1, y1] = getCanvasPoint(canvas, x1, y1);
+    [x2, y2] = getCanvasPoint(canvas, x2, y2);
+    console.log(x1, y1, x2, y2);
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.stroke();
     ctx.closePath();
 }
 
-function drawStation(ctx, bdColor, bgColor, x, y, width, height, direction) {
+function drawStation(canvas, bdColor, bgColor, x, y, width, height, direction) {
+    let ctx = canvas.getContext('2d')
     ctx.strokeStyle = bdColor;
     ctx.fillStyle = bgColor;
+    [x, y] = getCanvasPoint(canvas, x, y);
+    [width, height] = getCanvasPoint(canvas, width, height);
     ctx.beginPath();
     ctx.moveTo(x-width/2, y-height/2);
     ctx.lineTo(x+width/2, y-height/2);
     ctx.lineTo(x+width/2, y+height/2);
-    ctx.lineTo(x-width/2, y-height/2);
+    ctx.lineTo(x-width/2, y+height/2);
     ctx.lineTo(x-width/2, y-height/2);
     ctx.closePath();
     ctx.stroke();
     ctx.fill();
 }
 
-function drawCamp(ctx, bdColor, bgColor, x, y, r, direction) {
+function drawCamp(canvas, bdColor, bgColor, x, y, r, direction) {
+    let ctx = canvas.getContext('2d');
+    [x, y] = getCanvasPoint(canvas, x, y);
+    r = 2*r;
     ctx.strokeStyle = bdColor;
     ctx.fillStyle = bgColor;
     ctx.beginPath();
@@ -54,9 +99,13 @@ function drawCamp(ctx, bdColor, bgColor, x, y, r, direction) {
 }
 
 function getCanvasPoint (canvas, x, y) {
-    let rect = canvas.getBoundingClientRect();
-    return {
-        x: 2 * (x - rect.left), //canvas 显示大小缩放为实际大小的 50%。为了让图形在 Retina 屏上清晰
-        y: 2 * (y - rect.top),
-    };
+    return [
+        2 * (x - 0), //canvas 显示大小缩放为实际大小的 50%。为了让图形在 Retina 屏上清晰
+        2 * (y - 0)
+    ];
+}
+
+function Point2D(x, y) {
+    this.x = x;
+    this.y = y;
 }
